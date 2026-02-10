c108bb4eb25831de1daab642bb49790680a9e7a4
christian nicholas <143601631+nicholasteslaaa@users.noreply.github.com>
2026-02-10 00:04:36 -0500

    membuat timer pada ROI

--- web.py
+++ web.py
@@ -12,10 +12,6 @@ CORS(app)
 
 
 ROI = [] 
-ROI_FRAME = {} 
-
-
-
 
 class CameraStream:
     def __init__(self):
@@ -47,38 +43,42 @@ cam_manager = CameraStream().start()
 
 def generate_frame():
     prev_time = 0
+    global ROI
     while True:
         frame = cam_manager.get_frame()
-
-
-        frame = cv2.flip(frame, 1)
         
-        new_roi_frames = {}
         for idx in range(len(ROI)):
-            minX, minY, maxX, maxY = ROI[idx]
+            minX, minY, maxX, maxY = ROI[idx]["bbox"]
             cropped = frame[minY:maxY, minX:maxX].copy()
-            new_roi_frames[idx] = cropped
+            ROI[idx]["frame"] = cropped
         
-        global ROI_FRAME
-        ROI_FRAME = new_roi_frames
-
         curr_time = time.time()
         fps = 1 / (curr_time - prev_time) if (curr_time - prev_time) > 0 else 0
         prev_time = curr_time
         
         cv2.putText(frame, f"FPS: {int(fps)}", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
         cv2.putText(frame, f"ROIs Active: {len(ROI)}", (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-
-        # frame = AI.detect(frame)["frame"]
+        
         ret, buffer = cv2.imencode('.jpg', frame)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
 
 def generate_roi_stream(idx):
+    global ROI
+    
+    curr_time = time.time()
     while True:
-        frame = ROI_FRAME.get(idx)
+        frame = ROI[idx]["frame"]
         if frame is not None:
-            frame = AI.detect(frame)["frame"]
+            detection = AI.detect(frame)
+            frame = detection["frame"]
+            if (detection["people"] > 0):
+                new_time = time.time()
+                delta_time = new_time - curr_time
+                curr_time = new_time
+                
+                ROI[idx]["timer"] += delta_time
+            
             ret, buffer = cv2.imencode('.jpg', frame)
             if ret:
                 yield (b'--frame\r\n'
@@ -95,12 +95,19 @@ def get_ROI():
                         mimetype='multipart/x-mixed-replace; boundary=frame')
     return "Missing chair_idx", 400
 
+@app.route("/get_ROI_timer", methods=["GET"])
+def get_ROI_timer():
+    chair_idx = request.args.get('chair_idx', type=int)
+    if chair_idx is not None:
+        return jsonify({"timer":"%.2f"%ROI[chair_idx]["timer"]})
+    return "Missing chair_idx", 400
+
 @app.route("/add_ROI", methods=["POST"])
 def add_ROI():
     data = request.get_json()
     try:
         coords = tuple(map(int, data["ROI"].strip().split(" ")))
-        ROI.append(coords)
+        ROI.append({"bbox" : coords,"frame":None, "timer":0})
         return jsonify({"status": "success", "index": len(ROI) - 1})
     except Exception as e:
         return jsonify({"error": str(e)}), 400
@@ -109,20 +116,18 @@ def add_ROI():
 def pop_ROI():
     data = request.get_json()
     bbox = data["ROI"]
-
+    
     for i in range(len(ROI)):
-        x1,y1,x2,y2 = ROI[i]
+        x1,y1,x2,y2 = ROI[i]["bbox"]
         if (bbox == f"{x1} {y1} {x2} {y2}"):
             ROI.pop(i)
-            ROI_FRAME.pop(i)
         
-    
     return jsonify({"status": "success", "index": len(ROI) - 1})
 
 @app.route("/list_ROIs", methods=["GET"])
 def list_ROIs():
     # Returns a list of strings "x1 y1 x2 y2"
-    return jsonify({"rois": [f"{r[0]} {r[1]} {r[2]} {r[3]}" for r in ROI]})
+    return jsonify({"rois": [f"{r['bbox'][0]} {r['bbox'][1]} {r['bbox'][2]} {r['bbox'][3]}" for r in ROI]})
 
 @app.route('/cam_feed')
 def video_feed():
